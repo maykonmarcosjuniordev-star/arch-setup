19877e65860abb9d11f1f9d6c669e34f482fff7c
maykonmarcosjuniordev-star <maykon.marcos.junior.dev@gmail.com>
2025-11-11 09:13:46 -0300

    initial hyprland config

--- src/hyprland.sh
+++ src/hyprland.sh
@@ -6,9 +6,20 @@ yay -Sy --needed --noconfirm - < apps/hyprland.list
 # enabling services
 echo "enabling hyprland-related services"
 systemctl --user enable --now hyprlux.service
+systemctl --user enable --now hyprpolkitagent
 
-# Add these variables to your Hyprland config:
-env = LIBVA_DRIVER_NAME,nvidia
-env = __GLX_VENDOR_LIBRARY_NAME,nvidia
-env = ELECTRON_OZONE_PLATFORM_HINT,auto
-env = NVD_BACKEND,direct
+
+# creating symlinks for hyprland config
+echo "creating symlinks for hyprland config"
+mkdir -p ~/.config/hypr
+ln -sfn ~/arch-setup/config/hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
+ln -sfn ~/arch-setup/config/hyprland/waybar ~/.config/waybar
+chmod +x ~/arch-setup/config/hyprland/waybar/powermenu.sh
+ln -sfn ~/arch-setup/config/hyprland/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
+# setting wallpaper
+echo "setting wallpaper"
+ln -sfn ~/arch-setup/wallpapers/ ~/Pictures/Wallpapers
+
+echo "removing needless kitty installation"
+yay -Rns --noconfirm kitty
+

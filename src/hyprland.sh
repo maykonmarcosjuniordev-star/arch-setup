6d4c86b6cd80aab8f5c9f10336864bd57ddb736c
maykonmarcosjuniordev-star <maykon.marcos.junior.dev@gmail.com>
2025-12-09 06:27:05 -0300

    properly symlink of hyprpanel

--- src/hyprland.sh
+++ src/hyprland.sh
@@ -6,14 +6,15 @@ paru -Sy --needed --noconfirm - < ~/arch-setup/apps/hyprland.list
 
 # creating symlinks for hyprland config
 echo "creating symlinks for hyprland config"
-mkdir -p ~/.config/hypr
-ln -sfn ~/arch-setup/config/hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
-ln -sfn ~/arch-setup/config/hyprland/conf ~/.config/hypr/conf
-ln -sfn ~/arch-setup/config/hyprland/waybar ~/.config/waybar
+rm -rf ~/.config/hyprpanel
+ln -sfn ~/arch-setup/config/hyprland/hyprpanel ~/.config/
 ln -sfn ~/arch-setup/config/hyprland/wofi ~/.config/wofi
-ln -sfn ~/arch-setup/config/hyprland/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
+mkdir -p ~/.config/hypr
+ln -sfn ~/arch-setup/config/hyprland/conf ~/.config/hypr/conf
+ln -sfn ~/arch-setup/config/hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
 ln -sfn ~/arch-setup/config/hyprland/hyprlock.conf ~/.config/hypr/hyprlock.conf
 ln -sfn ~/arch-setup/config/hyprland/hypridle.conf ~/.config/hypr/hypridle.conf
+ln -sfn ~/arch-setup/config/hyprland/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
 mkdir -p ~/.config/hypr/wl-kbptr
 ln -sfn ~/arch-setup/config/hyprland/wl-kbptr.conf ~/.config/hypr/wl-kbptr/config
 mkdir -p ~/.config/sunsetr
